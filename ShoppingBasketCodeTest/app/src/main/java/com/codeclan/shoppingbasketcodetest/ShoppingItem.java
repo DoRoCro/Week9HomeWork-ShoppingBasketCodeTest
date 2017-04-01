2818983e2b360168fd288f6d70d98c8f71713620
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-01 18:34:28 +0100

    Define outline tests for shopping basket class

--- ShoppingBasketCodeTest/app/src/main/java/com/codeclan/shoppingbasketcodetest/ShoppingItem.java
+++ ShoppingBasketCodeTest/app/src/main/java/com/codeclan/shoppingbasketcodetest/ShoppingItem.java
@@ -6,6 +6,10 @@ package com.codeclan.shoppingbasketcodetest;
 
 public class ShoppingItem {
 
+    // considered option of making this an interface or abstract class
+    // but at the moment don't have sufficient reason to consider lots of different
+    // 'Basketables' based on the brief.
+
     private String description;
     private Integer price;
 
---
+++ ShoppingBasketCodeTest/app/src/test/java/com/codeclan/shoppingbasketcodetest/ShoppingBasketTest.java
@@ -0,0 +1,29 @@
+package com.codeclan.shoppingbasketcodetest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by DRC on 01/04/2017.
+ */
+
+public class ShoppingBasketTest {
+
+    ShoppingBasket basket;
+
+    @Before
+    public void before(){}
+
+    @Test
+    public void canAddItemToBasket(){}
+
+    @Test
+    public void canRemoveItemFromBasket(){}
+
+    @Test
+    public void canEmptyBasket(){}
+
+    @Test
+    public void canCountManyOfTheSameItemInTheBasket(){}
+
+}
